9145d12ca0aacddbfa5d92edf650cb308032de50
Daniel Blackburn <blackburnd@gmail.com>
2025-12-03 12:29:15 -0600

    Release version 1.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pypgsvg",
-    version="1.2.1",
+    version="1.2.2",
     description="Python ERD Generator from SQL dumps using Graphviz",
     author="blackburnd@gmail.com",
     packages=find_packages(),
